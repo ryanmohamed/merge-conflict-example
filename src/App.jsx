1d04bb38898a357c7d6ec1fd32303e0b576bb706
Ryan <reyaznyc@gmail.com>
2023-08-04 01:24:14 -0400

    added button api stuff

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageData, setImageData] = useState([]);
 
   return (
     <>
@@ -18,12 +18,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="btn" onClick={async () => {
+          const res = await fetch("https://picsum.photos/v2/list", { method: "GET" })
+          if (res.status !== 200) return;
+          const data = await res.json(); // array of objects
+          setImageData(data)
+        }}>
+          Get images
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="mt">
+          { imageData.map((image, idx) => (
+            <img className="img" src={image?.download_url} alt="image" key={idx}/>
+          )) }
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
